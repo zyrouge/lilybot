686d3e1e4b8b8efacf9d1b4f18c6ccd3710abf50
ZYROUGE <50770114+zyrouge@users.noreply.github.com>
2019-09-13 06:23:27 +0530

    Update bot.js

--- bot.js
+++ bot.js
@@ -1,18 +1,3 @@
-const http = require('http');
-const express = require('express');
-const app = express();
-app.get("/", (request, response) => {
-  console.log(Date.now() + " Ping Received");
-  response.sendStatus(200);
-});
-app.listen(process.env.PORT);
-setInterval(() => {
-  http.get(`http://${process.env.PROJECT_DOMAIN}.glitch.me/`);
-}, 280000);
-
-
-var keepAlive = require("node-keepalive");
-keepAlive({}, app);
 
 /*
  * Discord Bot Maker Bot
